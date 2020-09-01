d5290e2869aee61f47442e3a7733036b6feb0d97
darkster663 <emilp988@gmail.com>
2020-09-01 13:49:53 +0300

    module 4 assignment solution final

---
+++ module4-solution/src/menuapp/main-itemlist.controller.js
@@ -0,0 +1,15 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.controller('ItemListController', ItemListController);
+
+ItemListController.$inject = ['$stateParams', 'items'];
+function ItemListController($stateParams, items) {
+  var itemList = this;
+  itemList.items = items;
+  itemList.selectedCategory = $stateParams.categoryId;
+}
+
+
+})();
